257095ec2c67d30356fdb8a1365ea7936d335578
David Fox <dsf@seereason.com>
2017-11-22 07:49:49 -0800

    Rename package happstack-authenticate -> happstack-auth

--- debian/Debianize.hs
+++ debian/Debianize.hs
@@ -14,8 +14,6 @@ main = performDebianization customize
         debianDefaults
         -- These belong in cabal-debian/src/Debian/Debianize/Details.hs.  But as of
         -- cabal-debian 4.31.8 repeating them does no harm.
-        mapCabal (mkPackageName "happstack-authenticate") (DebBase "happstack-authenticate")
-        splitCabal (mkPackageName "happstack-authenticate") (DebBase "happstack-authenticate-0") (mkVersion [2])
         -- (debInfo . overrideDebianNameBase) .= Just (DebBase "happstack-authenticate-0")
         (debInfo . sourceFormat) .= Native3
         (debInfo . flags . cabalFlagAssignments) %= (Set.insert (mkFlagName "migrate", True))
